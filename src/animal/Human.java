84254566c2728ded1f8a8d698f44fd1f052c5d03
ooooosuke <ooosukefoool@gmail.com>
2024-02-28 22:06:53 +0900

    課題提出

---
+++ src/animal/Human.java
@@ -0,0 +1,23 @@
+package animal;
+
+public class Human extends Animal implements Thinkable {
+
+    private String hobby;
+
+    public Human(String name, int age,String hobby) {
+        super(name,age);
+        this.hobby = hobby;
+
+    }
+
+   public void think() {
+       System.out.println("私は、"+hobby+"について考えています。");
+    }
+   public String getHobby() {
+       return hobby;
+   }
+   public String setHobby() {
+       return hobby;
+   }
+
+}
